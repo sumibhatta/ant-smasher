f62936b9665947e893e58f61a7602397958d3260
sumibhatta <sumibhatta37@gmail.com>
2021-06-09 07:44:03 +0545

    Fix: Smashed Ant

--- js/smash.js
+++ js/smash.js
@@ -1,27 +1,22 @@
 canvas.onclick = (event) => {
-    // console.log('clicked')
     let clickedX = event.clientX;
     let clickedY = event.clientY;
 
-    clickedX -= canvas.offsetLeft;
-    clickedY -= canvas.offsetTop;
-    // console.log(clickedX, clickedY)
 
+    //Need to fix not in terms of x and y but whole diameter
     for (let i = 0; i < ants.length; i++) {
-        d1 = ants[i].x - clickedX;
-        // console.log(d1)
-        // console.log(ants[1].x, ants[1].y)
-        d2 = ants[i].y - clickedY;
-        // console.log(d1, d2)
-        d = Math.sqrt(d1**2+ d2**2);
-        // console.log(RADIUS-d)
-        // console.log(d)
-        if (d < RADIUS) {
-            // console.log(ants)
+        let ant = ants[i];
+        // console.log("x" + "" + ant.x + " " + "y" + " " + ant.y)
+        if (clickedX >= ant.x &&
+            clickedX <= (ant.x + (RADIUS * 2) + 20) &&
+            clickedY >= ant.y &&
+            clickedY <= (ant.y + (RADIUS * 2) + 20)
+        ) {
             ants.splice(i, 1);
-            // console.log(ants)
         }
     }
 
-}
+    console.log(clickedX, clickedY)
 
+
+}
